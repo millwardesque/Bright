8209e3e9404c48c654cdb3778fc22124bf504ba4
Christopher Millward <cmillward@millwardesque.com>
2016-06-23 06:36:12 -0400

    Initial commit.

---
+++ Assets/Scripts/Bright.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bright : MonoBehaviour {
+    public Light prefabLight;
+
+    public void CreateLight() {
+        Light newLight = Instantiate<Light>(prefabLight);
+        newLight.transform.position = transform.position;
+    } 
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+    public Bright bright;
+    public float lightSpeed = 1f;
+    public GameObject centreCamera;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+    // Update is called once per frame
+    void Update() {
+        if (Input.GetButtonDown("Fire1")) {
+            // @TODO Raycast from camera to find intersection point, then change Bright velocity to track to that point.
+            bright.GetComponent<Rigidbody>().velocity = lightSpeed * centreCamera.transform.forward;
+        }
+    }
+}
